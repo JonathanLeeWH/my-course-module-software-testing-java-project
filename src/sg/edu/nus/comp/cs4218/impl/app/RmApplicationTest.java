1081a3f5c2534b60d891ee400bba348693860214
JonathanLeeWH <leeweihao1996@outlook.com>
2020-02-08 20:55:47 +0800

    Added RmApplicationTest.java with removeFileOnly() method unit test when file exists and when file does not exist

---
+++ src/sg/edu/nus/comp/cs4218/impl/app/RmApplicationTest.java
@@ -0,0 +1,69 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+import sg.edu.nus.comp.cs4218.exception.RmException;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RmApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void removeFileOnly_fileExists_fileIsDeleted(@TempDir Path tempDir) throws IOException, RmException {
+        RmApplication rmApplication = new RmApplication();
+        Path fileToBeDeleted = tempDir.resolve("1.txt");
+        List<String> lines = Arrays.asList("1", "2", "3");
+        Files.write(fileToBeDeleted, lines);
+
+        assertTrue(Files.exists(fileToBeDeleted));
+
+        rmApplication.removeFileOnly(fileToBeDeleted.toFile());
+
+        assertFalse(Files.exists(fileToBeDeleted));
+    }
+
+    @Test
+    void removeFileOnly_fileDoesNotExist_exceptionThrown(@TempDir Path tempDir) throws IOException, RmException {
+        RmApplication rmApplication = new RmApplication();
+        Path fileToBeDeleted = tempDir.resolve("1.txt");
+
+        assertFalse(Files.exists(fileToBeDeleted)); // check to ensure file does not exist initially.
+
+        Exception exception = assertThrows(RmException.class, () -> {
+            rmApplication.removeFileOnly(fileToBeDeleted.toFile());
+        });
+    }
+
+    @Test
+    void removeFileAndEmptyFolderOnly() {
+    }
+
+    @Test
+    void removeFilesAndFolderContent() {
+    }
+
+    @Test
+    void run() {
+    }
+}
