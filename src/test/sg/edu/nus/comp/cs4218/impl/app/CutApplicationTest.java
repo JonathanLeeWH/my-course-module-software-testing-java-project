11c21984100323362bc25506b376d5881e9671f8
jjlee050 <josephlee050@gmail.com>
2020-02-14 13:45:10 +0800

    Add 1 success test cases.

--- src/test/sg/edu/nus/comp/cs4218/impl/app/CutApplicationTest.java
+++ src/test/sg/edu/nus/comp/cs4218/impl/app/CutApplicationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import sg.edu.nus.comp.cs4218.exception.CutException;
 import sg.edu.nus.comp.cs4218.impl.app.CutApplication;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -19,14 +20,14 @@ class CutApplicationTest {
     private CutApplication cutApplication;
     private String[] defaultCutArgs;
     private InputStream stdin;
-    private OutputStream stdout;
+    private OutputStream outputStream;
 
     @BeforeEach
     public void setUp() {
         cutApplication = new CutApplication();
         stdin = System.in;
-        stdout = System.out;
         defaultCutArgs = Arrays.asList("-c","8").toArray(new String[1]);
+        outputStream = new ByteArrayOutputStream();
     }
 
     @AfterEach
@@ -35,7 +36,7 @@ class CutApplicationTest {
 
     @Test
     void testRunNullArgs() {
-       Throwable thrown = assertThrows(CutException.class, () -> cutApplication.run(null, stdin, stdout));
+       Throwable thrown = assertThrows(CutException.class, () -> cutApplication.run(null, stdin, outputStream));
        assertEquals(thrown.getMessage(), CutApplication.COMMAND + ": " + ERR_NULL_ARGS);
     }
 
@@ -55,5 +56,12 @@ class CutApplicationTest {
     void testCutFromFilesNullFile() {
         Throwable thrown = assertThrows(CutException.class, () -> cutApplication.cutFromFiles(false, false, false, 1, 2, null));
         assertEquals(thrown.getMessage(), CutApplication.COMMAND + ": "  + ERR_GENERAL);
+    }
+
+    @Test
+    void testRunSuccess() throws CutException {
+        //Use a sample test file.
+        cutApplication.run(Arrays.asList("-c", "6", "README.md").toArray(new String[3]), stdin, outputStream);
+        assertEquals("2\na\nr\n", outputStream.toString());
     }
 }
