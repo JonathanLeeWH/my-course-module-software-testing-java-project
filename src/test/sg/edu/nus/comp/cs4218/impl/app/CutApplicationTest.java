3ed3d784a278ce6385d9d404c6cbcc31d17ebbbb
jjlee050 <josephlee050@gmail.com>
2020-02-16 21:00:13 +0800

    Add test cases
    I decide to change from using pair-wise testing to category parition testing as pair-wise testing is not exhaustive enough to consider the 2 given interface. Total 84 test cases.

--- src/test/sg/edu/nus/comp/cs4218/impl/app/CutApplicationTest.java
+++ src/test/sg/edu/nus/comp/cs4218/impl/app/CutApplicationTest.java
@@ -34,6 +34,10 @@ class CutApplicationTest {
     public void tearDown() {
     }
 
+    /**
+     * Test cut application with run().
+     */
+
     @Test
     void testRunNullArgs() {
        Throwable thrown = assertThrows(CutException.class, () -> cutApplication.run(null, stdin, outputStream));
@@ -47,49 +51,280 @@ class CutApplicationTest {
     }
 
     @Test
-    void testCutFromStdinNullInputStream() {
-        Throwable thrown = assertThrows(CutException.class, () -> cutApplication.cutFromStdin(false, false, false, 1, 2, null));
-        assertEquals(thrown.getMessage(), CutApplication.COMMAND + ": "  + ERR_NULL_STREAMS);
-    }
-
-    @Test
-    void testCutFromFilesNullFile() {
-        Throwable thrown = assertThrows(CutException.class, () -> cutApplication.cutFromFiles(false, false, false, 1, 2, null));
-        assertEquals(thrown.getMessage(), CutApplication.COMMAND + ": "  + ERR_GENERAL);
-    }
-
-    @Test
     void testRunSuccess() throws CutException {
         //Use a sample test file.
         cutApplication.run(Arrays.asList("-c", "6", "README.md").toArray(new String[3]), stdin, outputStream);
         assertEquals("2\na\nr\n", outputStream.toString());
     }
-    
-    
+
+    /**
+     * Test cut application using cutFromFiles()
+     */
+    // Erronorous Test cases (13 cases)
     @Test
-    void testCutUsingCharPosAndCommaSeparatedNumWithASingleFile() { }
+    void testCutFromFilesWithoutAnyPosFlags() { }
 
     @Test
-    void testCutUsingBytePosAndCommaSeparatedNumWithMultipleFiles() { }
+    void testCutFromFilesUsingByteAndCharPos() { }
 
     @Test
-    void testCutUsingCharPosAndNumRangeWithStdin() { }
+    void testCutFromFilesUsingBytePosAnd2CommaSeparatedNumAndASingleFileWithStartNumLessThanZero() { }
 
     @Test
-    void testCutUsingBytePosAndNumRangeWithASingleFile() { }
+    void testCutFromFilesUsingCharPosAnd2CommaSeparatedNumAndASingleFileWithEndNumLessThanZero() { }
 
     @Test
-    void testCutUsingCharPosAndSingleNumWithMultipleFiles() { }
+    void testCutFromFilesUsingCharPosAnd2CommaSeparatedNumAndASingleFileWithStartNumIsNotAInteger() { }
 
     @Test
-    void testCutUsingBytePosAndSingleNumWithStdin() { }
+    void testCutFromFilesUsingBytePosAnd2CommaSeparatedNumAndASingleFileWithEndNumIsNotAInteger() { }
 
     @Test
-    void testCutUsingCharPosAndCommaSeparatedNumWithStdin() { }
+    void testCutFromFilesUsingBytePosAndSingleNumAndASingleFileWithNumLessThanZero() { }
 
     @Test
-    void testCutUsingBytePosAndNumRangeWithMultipleFiles() { }
+    void testCutFromFilesUsingBytePosAndSingleNumAndASingleFileWithNumIsNotAInteger() { }
 
     @Test
-    void testCutUsingCharPosAndSingleNumWithASingleFile() { }
+    void testCutFromFilesUsingCharPosAndNumRangeAndASingleFileWithFileNotFoundInDir() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndNumRangeAndMultipleFilesWithAtLeastOneFileNotFoundInDir() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndNumRangeAndASingleFileWithInvalidFilename() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndNumRangeAndMultipleFilesWithAtLeastOneInvalidFilename() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndSingleNumAndNullFile() {
+        Throwable thrown = assertThrows(CutException.class, () -> cutApplication.cutFromFiles(false, false, false, 1, 2, null));
+        assertEquals(thrown.getMessage(), CutApplication.COMMAND + ": "  + ERR_GENERAL);
+    }
+
+    // Single Test cases (1 case)
+    @Test
+    void testCutFromFilesUsingBytePosAndSingleNumAndEmptyFilenameString() { }
+
+    // Positive test cases (50 cases)
+    @Test
+    void testCutFromFilesUsingBytePosAnd2CommaSeparatedNumAndValidSingleFileWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAnd2CommaSeparatedNumUnsortedAndValidSingleFileWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndNumRangeAndValidSingleFileWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndNumRangeAndValidSingleFileWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndSingleNumAndValidSingleFile() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAnd2CommaSeparatedNumAndValidSingleFileWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAnd2CommaSeparatedNumUnsortedAndValidSingleFileWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndNumRangeAndValidSingleFileWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndNumRangeAndValidSingleFileWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndSingleNumAndValidSingleFile() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAnd2CommaSeparatedNumAndValidDistinctFilesWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAnd2CommaSeparatedNumUnsortedAndValidDistinctFilesWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndNumRangeAndValidDistinctFilesWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndNumRangeAndValidDistinctFilesWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndSingleNumAndValidDistinctFiles() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAnd2CommaSeparatedNumAndValidDistinctFilesWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAnd2CommaSeparatedNumUnsortedAndValidDistinctFilesWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndNumRangeAndValidDistinctFilesWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndNumRangeAndValidDistinctFilesWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndSingleNumAndValidDistinctFiles() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAnd2CommaSeparatedNumAndFileIsDashWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAnd2CommaSeparatedNumUnsortedAndFileIsDashWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndNumRangeAndFileIsDashWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndNumRangeAndFileIsDashWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndSingleNumAndFileIsDash() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAnd2CommaSeparatedNumAndFileIsDashWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAnd2CommaSeparatedNumUnsortedAndFileIsDashWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndNumRangeAndFileIsDashWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndNumRangeAndFileIsDashWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndSingleNumAndFileIsDash() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAnd2CommaSeparatedNumAndValidSimilarFilesWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAnd2CommaSeparatedNumUnsortedAndValidSimilarFilesWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndNumRangeAndValidSimilarFilesWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndNumRangeAndValidSimilarFilesWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndSingleNumAndValidSimilarFiles() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAnd2CommaSeparatedNumAndValidSimilarFilesWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAnd2CommaSeparatedNumUnsortedAndValidSimilarFilesWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndNumRangeAndValidSimilarFilesWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndNumRangeAndValidSimilarFilesWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndSingleNumAndValidSimilarFiles() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAnd2CommaSeparatedNumAndHavingDashBetweenMultipleFilesWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAnd2CommaSeparatedNumUnsortedAndHavingDashBetweenMultipleFilesWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndNumRangeAndHavingDashBetweenMultipleFilesWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndNumRangeAndHavingDashBetweenMultipleFilesWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingBytePosAndSingleNumAndHavingDashBetweenMultipleFiles() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAnd2CommaSeparatedNumAndHavingDashBetweenMultipleFilesWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAnd2CommaSeparatedNumUnsortedAndHavingDashBetweenMultipleFilesWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndNumRangeAndHavingDashBetweenMultipleFilesWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndNumRangeAndHavingDashBetweenMultipleFilesWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromFilesUsingCharPosAndSingleNumAndHavingDashBetweenMultipleFiles() { }
+
+
+    /**
+     * Test cut application using cutFromStdin()
+     */
+    // Erronorous Test cases (9 cases)
+    @Test
+    void testCutFromStdinWithoutAnyPosFlags() { }
+
+    @Test
+    void testCutFromStdinUsingByteAndCharPos() { }
+
+    @Test
+    void testCutFromStdinUsingBytePosAnd2CommaSeparatedNumAndValidInputStreamWithStartNumLessThanZero() { }
+
+    @Test
+    void testCutFromStdinUsingCharPosAnd2CommaSeparatedNumAndValidInputStreamWithEndNumLessThanZero() { }
+
+    @Test
+    void testCutFromStdinUsingCharPosAnd2CommaSeparatedNumAndValidInputStreamWithStartNumIsNotAInteger() { }
+
+    @Test
+    void testCutFromStdinUsingBytePosAnd2CommaSeparatedNumAndValidInputStreamWithEndNumIsNotAInteger() { }
+
+    @Test
+    void testCutFromStdinUsingBytePosAndSingleNumAndValidInputStreamWithNumLessThanZero() { }
+
+    @Test
+    void testCutFromStdinUsingBytePosAndSingleNumAndValidInputStreamWithNumIsNotAInteger() { }
+
+    @Test
+    void testCutFromStdinUsingBytePosAndSingleNumAndNullInputStream() {
+        Throwable thrown = assertThrows(CutException.class, () -> cutApplication.cutFromStdin(false, false, false, 1, 2, null));
+        assertEquals(thrown.getMessage(), CutApplication.COMMAND + ": "  + ERR_NULL_STREAMS);
+    }
+
+    // Single Test cases (1 case)
+    @Test
+    void testCutFromStdinUsingBytePosAndSingleNumAndValidInputStreamWithAnEmptyInputStream() { }
+
+    // Positive Test cases (10 cases)
+    @Test
+    void testCutFromStdinUsingBytePosAnd2CommaSeparatedNumAndValidInputStreamWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromStdinUsingBytePosAnd2CommaSeparatedNumUnsortedAndValidInputStreamWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromStdinUsingBytePosAndNumRangeAndValidInputStreamWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromStdinUsingBytePosAndNumRangeAndValidInputStreamWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromStdinUsingBytePosAndSingleNumAndValidInputStream() { }
+
+    @Test
+    void testCutFromStdinUsingCharPosAnd2CommaSeparatedNumAndValidInputStreamWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromStdinUsingCharPosAnd2CommaSeparatedNumUnsortedAndValidInputStreamWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromStdinUsingCharPosAndNumRangeAndValidInputStreamWithStartNumLowerThanEndNum() { }
+
+    @Test
+    void testCutFromStdinUsingCharPosAndNumRangeAndValidInputStreamWithStartNumHigherThanEndNum() { }
+
+    @Test
+    void testCutFromStdinUsingCharPosAndSingleNumAndValidInputStream() { }
 }
