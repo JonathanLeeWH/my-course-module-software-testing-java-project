0b33bb3f2bb4e8b14fc302d75293643b55f39eed
jjlee050 <josephlee050@gmail.com>
2020-02-18 02:02:09 +0800

    Use our own pair instead of Javafx pair.
    Fix travis error.

---
+++ src/main/java/sg/edu/nus/comp/cs4218/impl/util/Pair.java
@@ -0,0 +1,19 @@
+package sg.edu.nus.comp.cs4218.impl.util;
+
+public class Pair<K, V> {
+    private K key;
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return this.key;
+    }
+
+    public V getValue() {
+        return this.value;
+    }
+}
