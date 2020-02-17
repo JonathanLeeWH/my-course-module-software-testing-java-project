228eb05311ce38f8c8f594112ab810966b4383d1
JonathanLeeWH <leeweihao1996@outlook.com>
2020-02-17 23:45:59 +0800

    Removed unused unit testing methods in PipeCommandTest.java

--- src/test/sg/edu/nus/comp/cs4218/impl/cmd/PipeCommandTest.java
+++ src/test/sg/edu/nus/comp/cs4218/impl/cmd/PipeCommandTest.java
@@ -1,28 +1,35 @@
 package sg.edu.nus.comp.cs4218.impl.cmd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import sg.edu.nus.comp.cs4218.exception.AbstractApplicationException;
 import sg.edu.nus.comp.cs4218.exception.ShellException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 class PipeCommandTest {
 
-    @Mock
-    private List<CallCommand> callCommands;
+    private List<CallCommand> mockCallCommandsList;
+
+    @BeforeEach
+    void setUp() {
+        mockCallCommandsList = mock(ArrayList.class);
+    }
 
     /**
-     * Tests evaluate method when call command throws a subclass of AbstractApplicationException.
+     * Tests evaluate method when call command throws AbstractApplicationException.
+     * Expected: Throws AbstractApplication.
      */
     @Test
     void evaluateWhenCallCommandThrowsAbsAppExceptionShouldThrowAbstractApplicationException() throws AbstractApplicationException, ShellException {
         CallCommand mockCallCommand = mock(CallCommand.class);
+        mockCallCommandsList.add(mockCallCommand);
+        verify(mockCallCommandsList).add(mockCallCommand);
 
         doThrow(mock(AbstractApplicationException.class)).when(mockCallCommand).evaluate(any(), any());
 
@@ -33,19 +40,18 @@ class PipeCommandTest {
 
     /**
      * Tests evaluate method when call command throws a ShellException.
+     * Expected: Throws ShellException.
      */
     @Test
     void evaluateWhenCallCommandThrowsShellExceptionShouldThrowShellException() throws AbstractApplicationException, ShellException {
         CallCommand mockCallCommand = mock(CallCommand.class);
+        mockCallCommandsList.add(mockCallCommand);
+        verify(mockCallCommandsList).add(mockCallCommand);
 
         doThrow(ShellException.class).when(mockCallCommand).evaluate(any(), any());
 
         assertThrows(ShellException.class, () -> {
             mockCallCommand.evaluate(any(), any());
         });
-    }
-
-    @Test
-    void getCallCommands() {
     }
 }
