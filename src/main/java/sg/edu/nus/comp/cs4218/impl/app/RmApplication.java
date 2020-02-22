dd0f15b63a4d45c280116a03833d83002e6da95f
JonathanLeeWH <leeweihao1996@outlook.com>
2020-02-22 17:09:23 +0800

    Removed unnecessary RmException catch in RmApplication.java as there is Exception catch present

--- src/main/java/sg/edu/nus/comp/cs4218/impl/app/RmApplication.java
+++ src/main/java/sg/edu/nus/comp/cs4218/impl/app/RmApplication.java
@@ -137,8 +137,6 @@ public class RmApplication implements RmInterface {
         } else {
             try {
                 remove(emptyFolder, recursive, fileNames);
-            } catch (RmException rmException) {
-                throw rmException;
             } catch(Exception e) {
                 throw (RmException) new RmException(e.getMessage()).initCause(e);
             }
