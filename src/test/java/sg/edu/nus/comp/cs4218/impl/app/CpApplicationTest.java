81ca8b3789b27da37c14976393bbbd4dcf0e1a81
JonathanLeeWH <leeweihao1996@outlook.com>
2020-02-23 10:59:49 +0800

    Added CpApplicationTest.java base

---
+++ src/test/java/sg/edu/nus/comp/cs4218/impl/app/CpApplicationTest.java
@@ -0,0 +1,20 @@
+package sg.edu.nus.comp.cs4218.impl.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CpApplicationTest {
+
+    @Test
+    void cpSrcFileToDestFile() {
+    }
+
+    @Test
+    void cpFilesToFolder() {
+    }
+
+    @Test
+    void run() {
+    }
+}
