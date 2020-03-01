d050176424f4dbf5da7c92a67c44a4dbcc7fc44e
Jason Leo <jas_p90@hotmail.com>
2020-03-02 02:23:07 +0800

    no message

--- src/test/java/sg/edu/nus/comp/cs4218/impl/util/ApplicationRunnerIT.java
+++ src/test/java/sg/edu/nus/comp/cs4218/impl/util/ApplicationRunnerIT.java
@@ -44,9 +44,9 @@ public class ApplicationRunnerIT {
     private static final String F1_CONTENT_SED = "helloThis is the content for file 1."
             + System.lineSeparator() + "helloThere are some content here."
             + System.lineSeparator() + "helloSome numbers: 50 1 2."
-            + System.lineSeparator() + "helloSome whitespace      ?><*&^%.";
+            + System.lineSeparator() + "helloSome whitespace    ><*&^%.?";
     private static final String F1_CONTENT_SORT = "Some numbers: 50 1 2." + System.lineSeparator() +
-            "Some whitespace      ?><*&^%." + System.lineSeparator() +
+            "Some whitespace    ><*&^%.?" + System.lineSeparator() +
             "There are some content here." + System.lineSeparator() +
             "This is the content for file 1.";
 
