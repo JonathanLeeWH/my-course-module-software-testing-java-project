5959b07a735ddb5af38e9d4c02d08ef40e8942aa
joel0111 <joel21_94@hotmail.com>
2020-03-23 01:37:18 +0800

    added test case for grep IT

--- src/test/java/sg/edu/nus/comp/cs4218/impl/app/GrepApplicationIT.java
+++ src/test/java/sg/edu/nus/comp/cs4218/impl/app/GrepApplicationIT.java
@@ -9,18 +9,20 @@ import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static sg.edu.nus.comp.cs4218.impl.util.StringUtils.STRING_NEWLINE;
 
 public class GrepApplicationIT {
     private static final String FILE_ONE_NAME = "fileOne";
     private static final String FILE_TWO_NAME = "fileTwo";
     private static final String FILE_FORMAT = ".txt";
     private static final String FILE_ONE_TEXT = "line one" + System.lineSeparator() + "line two" + System.lineSeparator() + "line three";
+    private static final String ONE_NAME_TEXT = FILE_ONE_NAME + FILE_FORMAT + ": line one" + System.lineSeparator()
+            + FILE_ONE_NAME + FILE_FORMAT + ": line two" + System.lineSeparator() + FILE_ONE_NAME + FILE_FORMAT + ": line three";
     private static final String LINE_ONE = "line one";
     private static final String INVALID = "invalid";
     private static final String LOWER_LINE = "line";
     private static final String UPPER_LINE = "Line";
-    private static File fileOne;
-    private static File fileTwo;
+    private static File fileOne, fileTwo;
     private static GrepApplication grepApplication;
     private static OutputStream outputStream, stdoutOne, stdoutTwo;
 
@@ -51,6 +53,18 @@ public class GrepApplicationIT {
         try(InputStream inputStream = new FileInputStream(FILE_ONE_NAME + FILE_FORMAT)) {
             grepApplication.run(args, inputStream, outputStream);
             assertEquals("3" + System.lineSeparator(), outputStream.toString());
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+
+    @Test
+    void runValidGrepArgumentWithoutFlagsShouldPrintLinesThatMatches() throws AbstractApplicationException, IOException {
+        String[] args = {LOWER_LINE, FILE_ONE_NAME + FILE_FORMAT};
+        outputStream = new ByteArrayOutputStream();
+        try(InputStream inputStream = new FileInputStream(FILE_ONE_NAME + FILE_FORMAT)) {
+            grepApplication.run(args, inputStream, outputStream);
+            assertEquals(FILE_ONE_TEXT + STRING_NEWLINE, outputStream.toString());
         } catch (IOException e) {
             e.getMessage();
         }
@@ -93,10 +107,22 @@ public class GrepApplicationIT {
      * Expected: throw Grep Exception
      */
     @Test
+    void testRunNullStdoutGrepArgumentShouldThrowGrepException() {
+        String[] args = {"-i", "-c", LOWER_LINE};
+        try(InputStream inputStream = new FileInputStream(FILE_ONE_NAME + FILE_FORMAT)) {
+            assertThrows(GrepException.class, () -> {
+                grepApplication.run(args, inputStream, null);
+            });
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+
+    @Test
     void runNoFilesAndNoStdinGrepArgumentShouldThrowGrepException() {
         String[] args = {"-i", "-c", LOWER_LINE};
         assertThrows(GrepException.class, () -> {
-            grepApplication.run(args, null, outputStream);
+            grepApplication.run(args, null, null);
         });
     }
 
@@ -170,5 +196,24 @@ public class GrepApplicationIT {
         InputStream inputStream = new FileInputStream(FILE_ONE_NAME + FILE_FORMAT);
         grepApplication.run(args, inputStream, outputStream);
         assertEquals("", outputStream.toString());
+    }
+
+    @Test
+    void testRunMethodWithFlagsAfterTheFileNames() throws FileNotFoundException, AbstractApplicationException {
+        String[] args = {"LinE", FILE_ONE_NAME + FILE_FORMAT, "-i", "-c"};
+        InputStream inputStream = new FileInputStream(FILE_ONE_NAME + FILE_FORMAT);
+        grepApplication.run(args, inputStream, outputStream);
+        String expected = "-i: No such file or directory" + STRING_NEWLINE +
+                "-c: No such file or directory" + STRING_NEWLINE;
+        assertEquals(expected, outputStream.toString());
+    }
+
+    @Test
+    void testRunMethodWithMultipleFileNames() throws FileNotFoundException, AbstractApplicationException {
+        String[] args = { "line", FILE_ONE_NAME + FILE_FORMAT, FILE_ONE_NAME + FILE_FORMAT};
+        InputStream inputStream = new FileInputStream(FILE_ONE_NAME + FILE_FORMAT);
+        grepApplication.run(args, inputStream, outputStream);
+        String expected = ONE_NAME_TEXT + STRING_NEWLINE + ONE_NAME_TEXT + STRING_NEWLINE;
+        assertEquals(expected, outputStream.toString());
     }
 }
