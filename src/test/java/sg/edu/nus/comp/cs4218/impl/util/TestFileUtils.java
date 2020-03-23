2055e84da7f7a630f18997bb51a30e8364098f17
jjlee050 <josephlee050@gmail.com>
2020-03-23 10:42:11 +0800

    Convert / to use File.separatorChar.

--- src/test/java/sg/edu/nus/comp/cs4218/impl/util/TestFileUtils.java
+++ src/test/java/sg/edu/nus/comp/cs4218/impl/util/TestFileUtils.java
@@ -1,5 +1,10 @@
 package sg.edu.nus.comp.cs4218.impl.util;
 
+import java.io.File;
+
 public class TestFileUtils {
-    public static final String TESTDATA_DIR = "src/test/java/sg/edu/nus/comp/cs4218/testdata/";
+    public static final String TESTDATA_DIR = "src" + File.separatorChar + "test" + File.separatorChar +
+            "java" + File.separatorChar + "sg" + File.separatorChar + "edu" + File.separatorChar +
+            "nus" + File.separatorChar + "comp" + File.separatorChar + "cs4218" + File.separatorChar +
+            "testdata" + File.separatorChar;
 }
